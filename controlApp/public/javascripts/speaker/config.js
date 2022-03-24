2d5c914a99b3667d4e2764d0da5630b1b5f22297
Michael Fabsich <michael.fabsich@gmx.at>
2022-03-24 17:38:40 +0100

    config changed

--- controlApp/public/javascripts/speaker/config.js
+++ controlApp/public/javascripts/speaker/config.js
@@ -1,5 +1,6 @@
 "use strict";
 let config = {
-    baseUrl: (window.location.protocol === 'https:' ? 'wss://' : 'ws://') + '192.168.0.96:1780'
+    //baseUrl: (window.location.protocol === 'https:' ? 'wss://' : 'ws://') + 'speaker_skplay.public.onsite.fonkwill.com:80'
+    baseUrl:  'wss://speaker_skplay.public.onsite.fonkwill.com'
 };
 //# sourceMappingURL=config.js.map
